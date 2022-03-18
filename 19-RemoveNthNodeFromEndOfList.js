78f79a80b68b2310daf1ff288d991f72dbf05cad
Hugo Aliaga Aguilar <h.aliaga.aguilar@gmail.com>
2022-03-18 13:32:21 +0100

    Added 19

--- 19-RemoveNthNodeFromEndOfList.js
+++ 19-RemoveNthNodeFromEndOfList.js
@@ -1,10 +1,14 @@
+"use strict";
 console.log('start');
 
 class LinkedList {
     constructor (array){
         this.value=array[0];
+        
         if (array.length>1){
-            this.next = new LinkedList(array.splice(1,array.length));
+            this.next = new LinkedList(array.splice(1,array.length),false);
+        } else {
+            this.next = null;
         }
     }
 }
@@ -20,18 +24,65 @@ LinkedList.prototype.toArray = function () {
     return newArray;
 }
 
-var removeNthFromEnd = function(head, n) {
-    let newArray = [];
-    
+var removeNthFromEnd2 = function(head, n) {
+    let listLength = -1;
+    let node = head;
+    node.value ? listLength++:'';
+    while(node){
+        listLength++;
+        node=node.next;
+    }
+    node =head;
+    console.log(listLength,n,listLength==n);
+    if (listLength<=n){
+        return node.next ? node.next : [];
+    }
+    for(let i=0;i< listLength-n-1;i++){
+        node=node.next;
+    }
+    console.log(node);
+    node.next = node.next ? node.next.next : null;
+    return head;
 };
 
-let myArray = [1,2,3,4,5];
-let myLL = new LinkedList(myArray);
-console.log(myLL.toArray());
+var removeNthFromEnd = function(head, n) {
+    let node = head;
+
+    for (let i=0;i<n;i++){
+        node = node ? node.next : null;
+    }
+    if(!node){
+        return head.next;
+    }
+    let node2 = head;
+
+    while (node.next){
+        node = node ? node.next : null;
+        node2 = node2.next;
+    }
+    node2.next = node2.next ? node2.next.next : null;
+    return head;
+};
+
+
 let tests=[];
 let solutions = [];
+tests.push([new LinkedList([1,2,3,4,5]),8]);
+solutions.push([1,2,3,5]);
+tests.push([new LinkedList([1]),1]);
+solutions.push([]);
+tests.push([new LinkedList([1,2]),1]);
+solutions.push([1]);
+tests.push([new LinkedList([1,2]),2]);
+solutions.push([2]);
 
-/* for(i=0;i<tests.length;i++){
+for(let i=0;i<tests.length;i++){
     let result = removeNthFromEnd(tests[i][0],tests[i][1]);
-    console.log('Test',i,'||',result.toArray(),solutions[i]);
-} */
+    if(result){
+        let newResult = result.toArray();
+
+    } else {
+        let newResult = result;
+    }
+    console.log('Test',i,'||',result,solutions[i]);
+}
