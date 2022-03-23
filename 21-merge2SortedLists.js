5bbd9c805c0fbd11119055f173c45f7cf2254080
Hugo Aliaga Aguilar <h.aliaga.aguilar@gmail.com>
2022-03-23 15:24:46 +0100

    Added 21

---
+++ 21-merge2SortedLists.js
@@ -0,0 +1,13 @@
+console.log('start');
+
+var mergeTwoLists = function(list1, list2) {
+    return list1;
+};
+
+let tests = [];
+let solutions = [];
+
+for (let i=0; i< tests.length;i++){
+    let result = ;
+    console.log('Test',i,'||',result,solutions[i],result==solutions[i] ? 'PASS' : 'FAIL');
+}
